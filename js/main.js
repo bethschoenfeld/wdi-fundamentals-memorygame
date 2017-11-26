25018026a623813ab9d48a47eaabafc3dbf99ac9
Bethany Schoenfeld <bethanyschoenfeld@Bethanys-MacBook-Air.local>
2017-11-26 14:44:45 -0500

    final version

--- js/main.js
+++ js/main.js
@@ -22,20 +22,35 @@ var cards = [
 ];
 var cardsInPlay = []
 
-var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
+var checkForMatch = function (playedCard, cardId) {
+	playedCard.setAttribute('src' , cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		} else {
+		alert("Sorry, try again.");
+	}
 }
 }
 
-var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank); 
-checkForMatch();
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank); 
+	checkForMatch(this, cardId);
 }
-flipCard(0);
-flipCard(1);
+// flipCard(0);
+// flipCard(1);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src' , 'images/back.png');
+		cardElement.setAttribute('data-id' , i );
+		cardElement.addEventListener('click' , flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+createBoard();
